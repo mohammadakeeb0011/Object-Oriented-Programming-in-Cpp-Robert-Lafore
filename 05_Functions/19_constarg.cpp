229f9337c235c04fc67af9b76ce154cd1fd60061
mohammadakeeb0011 <mohammadakeeb786@gmail.com>
2023-10-11 00:01:34 +0530

    11/10/2023

---
+++ 05_Functions/19_constarg.cpp
@@ -0,0 +1,16 @@
+//constarg.cpp
+//demonstrates constant function arguments
+void aFunc(int& a, const int& b); //declaration
+int main()
+{
+int alpha = 7;
+int beta = 11;
+aFunc(alpha, beta);
+return 0;
+}
+//--------------------------------------------------------------
+void aFunc(int& a, const int& b) //definition
+{
+a = 107; //OK
+b = 111; //error: can’t modify constant argument
+}
